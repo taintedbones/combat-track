f31abd727ee8075c74ecdbffd598578d70284532
taintedbones <kjust@csu.fullerton.edu>
2021-10-26 21:31:01 -0700

    Created actor editor components

---
+++ src/views/EditActor/EditActors.tsx
@@ -0,0 +1,18 @@
+import { Grid, Typography } from '@mui/material';
+import DataTable from '../../components/DataTable';
+import ActorToolbar from './components/ActorToolbar';
+import '../../styles/App.css';
+
+export default function EditActors() {
+  return (
+    <div className='container'>
+      <Grid container justifyContent='center' direction='row' spacing={2}>
+        <Typography variant='h4'>Actor Editor</Typography>
+        <Grid item xs={12}>
+          <DataTable />
+        </Grid>
+        <ActorToolbar />
+      </Grid>
+    </div>
+  );
+}
