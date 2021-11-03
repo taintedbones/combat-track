0ff72f4228a6be8f960adb5ab5f8039cfe3d2ed0
taintedbones <kjust@csu.fullerton.edu>
2021-11-02 20:16:29 -0700

    changed useScenarios to grab actors from specified scenario name

--- src/hooks/useDatabase.ts
+++ src/hooks/useDatabase.ts
@@ -1,6 +1,12 @@
-import { useState, useEffect } from "react";
-import { collection, getDocs, DocumentData } from "firebase/firestore/";
-import { database } from "../firebaseConfig";
+import { useState, useEffect } from 'react';
+import {
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  DocumentData,
+} from 'firebase/firestore/';
+import { database } from '../firebaseConfig';
 
 export const useActors = () => {
   const [error, setError] = useState(false);
@@ -11,10 +17,12 @@ export const useActors = () => {
     const fetchActors = async () => {
       setLoading(true);
       try {
-        const actorsCol = collection(database, "actors");
+        const actorsCol = collection(database, 'actors');
         const actorsSnapshot = await getDocs(actorsCol);
-        const actorsList = actorsSnapshot.docs.map((doc) => doc.data());
-        const actorsIdList = actorsList.map((actor, index) => Object.defineProperty(actor, "id", { value: index }));
+        const actorsList = actorsSnapshot.docs.map((doc) => doc.data()); // create list map of actors from snapshot
+        const actorsIdList = actorsList.map((actor, idx) =>
+          Object.defineProperty(actor, 'id', { value: idx })
+        );
         const sortedList = actorsIdList.sort((a, b) => {
           if (a.initiative === b.initiative) {
             return b.name - a.name;
@@ -22,7 +30,9 @@ export const useActors = () => {
             return b.initiative - a.initiative;
           }
         });
-        const temp = sortedList.map((actor, index) => Object.defineProperty(actor, "index", { value: index }));
+        const temp = sortedList.map((actor, index) =>
+          Object.defineProperty(actor, 'index', { value: index })
+        );
         // console.log(actorsIdList);
         setLoading(false);
         setActors(temp);
@@ -39,31 +49,102 @@ export const useActors = () => {
   return { error, loading, actors };
 };
 
+async function getListOfScenarios() {
+  const snapshot = await getDocs(collection(database, 'scenarios'));
+  let scenarioList: any[] = [];
+  snapshot.forEach((doc) => scenarioList.push(doc.id));
+  return scenarioList;
+}
+
+// get specific scenario (maybe turn this vv into specific)
 export const useScenarios = () => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [scenarios, setScenarios] = useState<DocumentData[]>([]);
+  const [scenarios, setScenarios] = useState<any[]>([]);
+  // const [scenarioNames, setScenarioNames] = useState<string[]>([]);
+
+  async function getActorFromRef(refPath) {
+    const docName = refPath.split('/')[1];
+    const docRef = doc(database, 'actors', docName);
+    const snapshot = await getDoc(docRef);
+    return snapshot.data();
+  }
 
   useEffect(() => {
-    const fetchActors = async () => {
+    const fetchScenario = async () => {
       setLoading(true);
       try {
-        const scenariosCol = collection(database, "scenarios");
-        const scenariosSnapshot = await getDocs(scenariosCol);
-        const scenariosList = scenariosSnapshot.docs.map((doc) => doc.data());
-        const scenariosIdList = scenariosList.map((actor, index) => ({ ...actor, id: index }));
-        // console.log(scenariosIdList);
+        const docRef = doc(database, 'scenarios', 'skeletons');
+        const scenarioSnapshot = await getDoc(docRef);
+        const actorRefsList = scenarioSnapshot.get('actors');
+
+        const actorList = await Promise.all(
+          actorRefsList.map(async (actorRef, idx) => {
+            try {
+              const actor = await getActorFromRef(actorRef.path);
+              return {
+                ...actor,
+                id: idx,
+              };
+            } catch (err) {
+              console.log(err);
+            }
+          })
+        );
+
+        setScenarios(actorList);
         setLoading(false);
-        setScenarios(scenariosIdList);
-        return scenariosIdList;
+
+        return actorList;
       } catch (error) {
         console.error(error);
         setLoading(false);
-        setError(true);
       }
     };
-    fetchActors();
-  }, [setError, setLoading, setScenarios]);
+    fetchScenario();
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchScenario = async () => {
+  //     setLoading(true);
+  //     try {
+  //       // create specific scenario reference
+  //       const docRef = doc(database, 'scenarios', scenarioName);
+  //       // get document from reference
+  //       const scenarioSnapshot = await getDoc(docRef);
+  //       // convert actors in array to actor objects
+  //       const actorRefsList = scenarioSnapshot.get('actors');
+  //       // console.log('actorRefs: ', actorRefsList);
+  //       // console.log('path 1: ', actorRefsList[0].path);
+
+  //       let actorList: any[] = [];
+
+  //       for (let i = 0; i < actorRefsList.length; i++) {
+  //         const temp = await getActorFromRef(actorRefsList[i].path);
+  //         if(temp !== undefined) {
+  //           Object.defineProperty(temp, "id", { value: i });
+  //           // console.log("temp: ", temp["id"]);
+  //           actorList.push(temp);
+  //         }
+  //       }
+
+  //       // console.log('actorList: ', actorList);
+  //       let scenarioNamesList = await getListOfScenarios();
+  //       // console.log(scenarioNamesList);
+
+  //       setLoading(false);
+  //       setScenarios(actorList);
+  //       setScenarioNames(scenarioNamesList);
+
+  //       return actorList;
+  //     } catch (error) {
+  //       console.error(error);
+  //       setLoading(false);
+  //       setError(true);
+  //     }
+  //   };
+  //   fetchScenario();
+  //   }, [scenarioName]);
 
   return { error, loading, scenarios };
 };
@@ -74,13 +155,16 @@ export const useUsers = () => {
   const [users, setUsers] = useState<DocumentData[]>([]);
 
   useEffect(() => {
-    const fetchActors = async () => {
+    const fetchUsers = async () => {
       setLoading(true);
       try {
-        const usersCol = collection(database, "users");
+        const usersCol = collection(database, 'users');
         const usersSnapshot = await getDocs(usersCol);
         const usersList = usersSnapshot.docs.map((doc) => doc.data());
-        const usersIdList = usersList.map((actor, index) => ({ ...actor, id: index }));
+        const usersIdList = usersList.map((actor, index) => ({
+          ...actor,
+          id: index,
+        }));
         // console.log(usersIdList);
         setLoading(false);
         setUsers(usersIdList);
@@ -91,7 +175,7 @@ export const useUsers = () => {
         setError(true);
       }
     };
-    fetchActors();
+    fetchUsers();
   }, [setError, setLoading, setUsers]);
 
   return { error, loading, users };
