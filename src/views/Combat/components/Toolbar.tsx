8058d4c9cf2942d3ca09c6f76861284342cde0b8
taintedbones <kjust@csu.fullerton.edu>
2021-11-02 20:14:25 -0700

    replaced select scenario w/ back button & changed function params

--- src/views/Combat/components/Toolbar.tsx
+++ src/views/Combat/components/Toolbar.tsx
@@ -1,48 +1,57 @@
-import { Button, Typography, Grid } from "@mui/material";
+import { Button, Typography, Grid } from '@mui/material';
 
-const typography = {
-  color: "white",
-  fontSize: "2rem",
-};
-
-export default function Toolbar({ roundNum, actorTurn, endTurn, onAddActor }) {
+export default function CombatToolbar({
+  onBackClicked,
+  currentTurnName,
+  roundNum,
+  endTurn,
+  onAddActor
+}) {
   return (
-    <Grid item container spacing={4}>
+    <Grid item container xs={12}>
       <Grid item container spacing={4}>
-        <Grid item xs={2}>
-          <Typography sx={typography}>Current Turn:</Typography>
+        <Grid item container spacing={4}>
+          <Grid item xs={2}>
+            <Typography variant="h5">Current Turn:</Typography>
+          </Grid>
+          <Grid container item xs={2}>
+            <Typography variant="h5">{`${currentTurnName}`}</Typography>
+          </Grid>
+          <Grid container item xs justifyContent="flex-end">
+            <Typography variant="h5">{`Round: ${roundNum}`}</Typography>
+          </Grid>
         </Grid>
-        <Grid container item xs={2}>
-          <Typography sx={typography}>{`${actorTurn}`}</Typography>
+        <Grid item xs>
+          <Button variant="contained" fullWidth onClick={onBackClicked}>
+            Back
+          </Button>
         </Grid>
-        <Grid container item xs justifyContent="flex-end">
-          <Typography sx={typography}>{`Round: ${roundNum}`}</Typography>
+        <Grid item xs>
+          {/* <Button variant="contained" fullWidth onClick={onAddActor}> */}
+          <Button variant="contained" fullWidth onClick={onAddActor}>
+            Add Actor
+          </Button>
         </Grid>
-      </Grid>
-      <Grid item xs>
-        <Button variant="contained" fullWidth onClick={onAddActor}>
-          Add Actor
-        </Button>
-      </Grid>
-      <Grid item xs>
-        <Button variant="contained" fullWidth>
-          Delete Actor
-        </Button>
-      </Grid>
-      <Grid item xs>
-        <Button variant="contained" fullWidth>
-          Select Scenario
-        </Button>
-      </Grid>
-      <Grid item xs>
-        <Button variant="contained" color="warning" fullWidth onClick={endTurn}>
-          End Turn
-        </Button>
-      </Grid>
-      <Grid item xs>
-        <Button variant="contained" color="error" fullWidth>
-          End Combat
-        </Button>
+        <Grid item xs>
+          <Button variant="contained" fullWidth>
+            Delete Actor
+          </Button>
+        </Grid>
+        <Grid item xs>
+          <Button
+            variant="contained"
+            color="warning"
+            fullWidth
+            onClick={endTurn}
+          >
+            End Turn
+          </Button>
+        </Grid>
+        <Grid item xs>
+          <Button variant="contained" color="error" fullWidth>
+            End Combat
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
