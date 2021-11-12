1d87cc5d4ccefdc4d75f4be49749953efdeab182
lddang1762 <lddang1762@csu.fullerton.edu>
2021-11-11 23:37:58 -0800

    initical commit

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,14 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // change details to be stored in env file instead of hard coded
 const firebaseApp = initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyBwzDeaFXRPFEqQhk1kWM2YqL7Pr6lL7aY",
   authDomain: "combat-track.firebaseapp.com",
   projectId: "combat-track",
   storageBucket: "combat-track.appspot.com",
   messagingSenderId: "725526804018",
-  appId: "1:725526804018:web:ca2510bb0726889eee6e2b"
+  appId: "1:725526804018:web:ca2510bb0726889eee6e2b",
 });
-
-
 
 export const database = getFirestore(firebaseApp);
